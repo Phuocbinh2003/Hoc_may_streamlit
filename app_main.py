a26edda1187fd1793dc2581854e234627d4e1b8f
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-13 10:01:20 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from giai_thua import main as GT
+from buoi2.tien_su_ly_du_lieu import tien_xu_ly_du_lieu as TXLDL
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('Giai thừa') 
+    ('Giai thừa','Tiền xử lý dữ liệu') 
 )
 if option == 'Giai thừa':
-    GT()  
+    GT()
+if option == 'Giai thừa':
+    TXLDL() 
 else:
     st.write("Vui lòng chọn một ứng dụng từ thanh điều hướng.")
