6fe91365dc7b339ac429a5e742c814baff59bc2e
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-17 07:09:17 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,14 +1,18 @@
 import streamlit as st
 from giai_thua import main as GT
 from buoi2.tien_su_ly_du_lieu import tien_xu_ly_du_lieu as TXLDL
+from buoi2.Data_Processing import classification_report as CR
+
 
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('Giai thừa','Tiền xử lý dữ liệu') 
+    ('Giai thừa','Tiền xử lý dữ liệu','Data Processing') 
 )
 if option == 'Giai thừa':
     GT()
 if option == 'Tiền xử lý dữ liệu':
     TXLDL() 
+if option == 'Data Processing':
+    CR() 
 else:
     st.write("Vui lòng chọn một ứng dụng từ thanh điều hướng.")
