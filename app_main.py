50efd30d0f5d20c2c68848237cb85625f818100f
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-19 20:07:20 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,15 +1,14 @@
 import streamlit as st
-from giai_thua import main as GT
+
 from buoi2.tien_su_ly_du_lieu import tien_xu_ly_du_lieu 
 from buoi2.Data_Processing import report as CR
 
 
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('Giai thừa','Tiền xử lý dữ liệu','Data Processing') 
+    ('Tiền xử lý dữ liệu','Data Processing') 
 )
-if option == 'Giai thừa':
-    GT()
+
 if option == 'Tiền xử lý dữ liệu':
     tien_xu_ly_du_lieu () 
 if option == 'Data Processing':
