1361a0e34c529b50598337198e26c7e05e6b8f6d
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-19 21:15:19 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -2,16 +2,18 @@ import streamlit as st
 
 from buoi2.tien_su_ly_du_lieu import tien_xu_ly_du_lieu 
 from buoi2.Data_Processing import report as CR
-
+from buoi3.Linear_Regression import bt_buoi3
 
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('Tiền xử lý dữ liệu','Data Processing') 
+    ('Tiền xử lý dữ liệu','Data Processing', 'Linear Regression') 
 )
 
 if option == 'Tiền xử lý dữ liệu':
     tien_xu_ly_du_lieu () 
 if option == 'Data Processing':
     CR() 
+if option == 'Linear Regression':
+    bt_buoi3()
 else:
     st.write("Vui lòng chọn một ứng dụng từ thanh điều hướng.")
