2bcda4ff15fdbf13f08325e98b7acf82d9b972e4
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-21 16:29:06 +0700

    Update tien_su_ly_du_lieu.py

--- buoi2/tien_su_ly_du_lieu.py
+++ buoi2/tien_su_ly_du_lieu.py
@@ -101,7 +101,7 @@ def train_test_size(df):
 
         st.success("✅ Dữ liệu đã được chia thành công!")
         
-    st.write(X_train.header())
+    st.dataframe(X_train.head())
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 def xu_ly_gia_tri_thieu(df):
