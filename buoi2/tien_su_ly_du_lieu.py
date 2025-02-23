d483c004a0c23e3eb0d994c9c17e80d88a1c3caf
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-24 03:45:10 +0700

    Update tien_su_ly_du_lieu.py

--- buoi2/tien_su_ly_du_lieu.py
+++ buoi2/tien_su_ly_du_lieu.py
@@ -581,7 +581,7 @@ def test():
     st.write(model)
     # Chuyển đổi list thành array
     X_train_input = np.array(X_train_input).reshape(1, -1)
-    st.write(X_train_input[0].dtype)
+    st.write(X_train_input)
     if np.all(np.isin(X_train_input, [0, 1])):
     # Nếu tất cả giá trị là 0 hoặc 1 thì không cần chuẩn hó
         X_train_input_final = X_train_input
