5207a47aeb986353a8afcecec57497eb682223e8
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-24 03:38:16 +0700

    Update tien_su_ly_du_lieu.py

--- buoi2/tien_su_ly_du_lieu.py
+++ buoi2/tien_su_ly_du_lieu.py
@@ -583,7 +583,7 @@ def test():
     X_train_input = np.array(X_train_input).reshape(1, -1)
     st.write(X_train_input)
     if np.all(np.isin(X_train_input, [0, 1])):
-    # Nếu tất cả giá trị là 0 hoặc 1 thì không cần chuẩn hóa
+    # Nếu tất cả giá trị là 0 hoặc 1 thì không cần chuẩn hó
         X_train_input_final = X_train_input
     else:
         # Nếu có giá trị khác 0 và 1, chuẩn hó
