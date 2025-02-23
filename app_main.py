cb0769b5747a61e3128afd42a4324345c99380aa
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-23 20:46:43 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 from buoi2.tien_su_ly_du_lieu import main 
-from buoi2.Data_Processing import report as CR
-from buoi3.Linear_Regression import bt_buoi3
+# from buoi2.Data_Processing import report as CR
+# from buoi3.Linear_Regression import bt_buoi3
 from buoi4.Classification import Classification
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
