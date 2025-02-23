e7d585ae2df031c25e7c70dcde2c7f24fddf8703
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-23 21:45:21 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from buoi2.tien_su_ly_du_lieu import main 
 from buoi4.Classification import Classification
+from test import Classification2 as ts
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('Linear Regression','Classification') 
+    ('Linear Regression','Classification','test') 
 )
 
 if option == 'Linear Regression':
     main() 
 if option == 'Classification':
     Classification() 
-
+if option == 'test':
+    ts() 
 else:
     st.write("Vui lòng chọn một ứng dụng từ thanh điều hướng.")
