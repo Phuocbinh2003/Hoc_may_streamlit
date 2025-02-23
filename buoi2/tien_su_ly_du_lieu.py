00f54114f58e46776de3ea56a026f797e507b682
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-24 06:57:19 +0700

    Update tien_su_ly_du_lieu.py

--- buoi2/tien_su_ly_du_lieu.py
+++ buoi2/tien_su_ly_du_lieu.py
@@ -588,9 +588,9 @@ def test():
     # Tạo mảng chỉ số của các phần tử khác 0 và 1
     for i in range(X_train_input.shape[1]):
         st.write(X_train_input[0,i])
-        if X_train_input[ i] != 0 and X_train_input[ i] != 1:  # Nếu giá trị không phải 0 hoặc 1
+        if X_train_input[0, i] != 0 and X_train_input[0, i] != 1:  # Nếu giá trị không phải 0 hoặc 1
             # Chuẩn hóa giá trị
-            X_train_input_final[i] = scaler.fit_transform(X_train_input[:, i].reshape(-1, 1)).flatten()
+            X_train_input_final[0, i] = scaler.fit_transform(X_train_input[:, i].reshape(-1, 1)).flatten()
 
 
     # if np.all(np.isin(X_train_input, [0, 1])):
@@ -601,7 +601,7 @@ def test():
     #     scaler = StandardScaler()
         # X_train_input_final = scaler.fit_transform(X_train_input.reshape(-1, 1)).flatten()
      # Chuẩn hóa các giá trị khác 0 và 1
-    scaler = StandardScaler()
+    #scaler = StandardScaler()
     # X_train_input_final[:, non_binary_indices] = scaler.fit_transform(X_train_input[:, non_binary_indices].T).T
 
         # Hiển thị kết quả
