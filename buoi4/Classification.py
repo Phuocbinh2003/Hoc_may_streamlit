3db5662760a1482814a1ea901f3395b113101f9a
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-23 15:36:19 +0700

    Update Classification.py

--- buoi4/Classification.py
+++ buoi4/Classification.py
@@ -131,8 +131,8 @@ def Classification():
     → Mọi điểm dữ liệu **phải nằm đúng phía** của siêu phẳng, đảm bảo phân loại chính xác.  
 
     **Trong đó:**  
-    - \( $$xi_i$$ \) : điểm dữ liệu đầu vào.  
-    - \( $$yi_i$$ \) : nhãn của điểm dữ liệu (\(+1\) hoặc \(-1\)).  
+    - \( $$xi$$ \) : điểm dữ liệu đầu vào.  
+    - \( $$yi$$ \) : nhãn của điểm dữ liệu (\(+1\) hoặc \(-1\)).  
 
     - **Khoảng cách từ một điểm đến siêu phẳng**:  
     $$d = \frac{|w \cdot x + b|}{||w||}$$  
