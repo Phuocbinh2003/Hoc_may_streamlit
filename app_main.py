fb999239736ae0e9c42d284ab1f4ae98eeee19cb
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-24 08:34:18 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from buoi2.tien_su_ly_du_lieu import main 
 from buoi4.Classification import Classification
-from test import Classification2 as ts
+from test import appptest as ts
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
     ('Linear Regression','Classification','test') 
