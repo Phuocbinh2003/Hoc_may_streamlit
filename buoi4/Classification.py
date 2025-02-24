d7f1a6c73c2240e102be758b8caa5f16e026ca2b
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-24 11:28:35 +0700

    Update Classification.py

--- buoi4/Classification.py
+++ buoi4/Classification.py
@@ -414,7 +414,7 @@ def preprocess_canvas_image(canvas_result):
         img = np.array(img, dtype=np.float32) / 255.0  # Chuẩn hóa về [0, 1]
         return img.reshape(1, -1)  # Chuyển thành vector 1D
     return None
-
+from streamlit_draw import draw
 # ✅ Chạy dự đoán
 def du_doan():
     st.header("✍️ Vẽ số để dự đoán")
@@ -448,7 +448,7 @@ def du_doan():
 
     # ✍️ Vẽ số
     st.subheader("🖌️ Vẽ số vào khung dưới đây:")
-    
+    result = draw(size=(150, 150), color="white", bg_color="black", stroke_width=10)
     canvas_result = st_canvas(
         fill_color="black",
         stroke_width=10,
