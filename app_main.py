66c21e1b5388db60251ef1da4c427b479529348f
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-25 23:53:29 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from buoi2.tien_su_ly_du_lieu import main 
 from buoi4.Classification import Classification
-from buoi5.Clustering_Algorithms import ClusteringAlgorithm 
+from buoi5.Clustering_Algorithms import ClusteringAlgorithms
 # from test import appptest as ts
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
@@ -14,7 +14,7 @@ if option == 'Classification MNIST':
     Classification() 
     
 if option == 'Clustering Algorithms':
-    ClusteringAlgorithm() 
+    ClusteringAlgorithms() 
 # if option == 'test':
 #     ts() 
 else:
