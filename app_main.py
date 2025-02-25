d0744789451bea3612bb07cdbf0d723cab4fc6e8
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-02-25 23:52:22 +0700

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from buoi2.tien_su_ly_du_lieu import main 
 from buoi4.Classification import Classification
+from buoi5.Clustering_Algorithms import ClusteringAlgorithm 
 # from test import appptest as ts
 option = st.sidebar.selectbox(
     'Chọn ứng dụng:',
-    ('Linear Regression','Classification MNIST') 
+    ('Linear Regression','Classification MNIST','Clustering Algorithms') 
 )
 
 if option == 'Linear Regression':
     main() 
 if option == 'Classification MNIST':
     Classification() 
+    
+if option == 'Clustering Algorithms':
+    ClusteringAlgorithm() 
 # if option == 'test':
 #     ts() 
 else:
