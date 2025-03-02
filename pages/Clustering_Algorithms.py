99c1fe037e22cef67bad1426cd7d3c653b35dcb4
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-02 10:22:12 +0700

    Create Clustering_Algorithms.py

---
+++ pages/Clustering_Algorithms.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from buoi5.Clustering_Algorithms import ClusteringAlgorithms
+
+st.title("🔍 Clustering Algorithms")
+st.write("Ứng dụng Clustering Algorithms đang chạy...")
+
+# Gọi hàm ClusteringAlgorithms từ module
+ClusteringAlgorithms()
