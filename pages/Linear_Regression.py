7240b747b521fd7c3d8dc8d54d9d490545788856
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-02 10:22:14 +0700

    Create Linear_Regression.py

---
+++ pages/Linear_Regression.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from buoi2.tien_su_ly_du_lieu import main
+
+st.title("📈 Linear Regression")
+st.write("Ứng dụng Linear Regression đang chạy...")
+
+# Gọi hàm main từ module
+main()
