917aeb0ed6d2c025e48ec0e968b66cb0205e8e16
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-06 04:54:56 +0700

    Update Classification.py

--- buoi4/Classification.py
+++ buoi4/Classification.py
@@ -25,8 +25,7 @@ def ly_thuyet_Decision_tree():
     """)
 
     # Hiển thị ảnh minh họa Decision Tree
-    st.image("buoi4/img1.png", caption="Ví dụ về cách Decision Tree phân chia dữ liệu", use_container_width=True)
-
+    st.image("buoi4/img1.png", caption="Ví dụ về cách Decision Tree phân chia dữ liệu")
     st.subheader("2️⃣ Các bước thực hiện trong Decision Tree")
     st.write("""
     **Bước 1: Tính Entropy của tập dữ liệu ban đầu**
