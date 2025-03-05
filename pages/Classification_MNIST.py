d64cff22aae512d14a8985d7657522ad17bdb87a
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-06 06:23:33 +0700

    Update Classification_MNIST.py

--- pages/Classification_MNIST.py
+++ pages/Classification_MNIST.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from buoi4.Classification import Classification
 
+if "last_page" in st.session_state and st.session_state.last_page != "Classification":
+    st.session_state.clear()  # Xóa toàn bộ session
+
+st.session_state.last_page = "Classification" 
+
 st.title("🔢 Classification MNIST")
 
 
