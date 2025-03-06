a4886c85da9aac9420204bfa88b68a615d665520
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-06 07:02:54 +0700

    Update Clustering_Algorithms.py

--- buoi5/Clustering_Algorithms.py
+++ buoi5/Clustering_Algorithms.py
@@ -359,10 +359,7 @@ def ly_thuyet_DBSCAN():
     ax.legend()
 
     # Hiển thị biểu đồ
-    fig, ax = plt.subplots(figsize=(2, 2))  # Giảm kích thước xuống
-
-    # col1, col2, col3 = st.columns([1, 5, 1])  # Cột giữa chiếm nhiều không gian hơn
-    # with col2:
+   
     st.pyplot(fig)
 
 
