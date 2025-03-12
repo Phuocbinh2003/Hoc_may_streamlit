7d1c3eeefd0e54986566757c631ffadf95aa9285
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-12 23:23:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ def explain_nn():
 
     ### 🔰 Kiến trúc cơ bản:
     """)
-    st.image("buoi7/img1.webp", caption="Một số hình ảnh từ MNIST Dataset", use_container_width=True)
+    st.image("buoi7/img1.webp", caption="", use_container_width=True)
     
     st.markdown("""
     ### 📌 Các thành phần chính:
@@ -35,7 +35,7 @@ def explain_nn():
 
     ### 🎯 Hàm kích hoạt (Activation Functions):
     """)
-    st.image("buoi7/img1.png", caption="Một số hình ảnh từ MNIST Dataset", use_container_width=True)
+    st.image("buoi7/img1.png", caption="", use_container_width=True)
     
     st.markdown("""
     ### 🔄 Quá trình lan truyền:
