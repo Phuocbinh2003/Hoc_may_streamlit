f0a71f0ab53a733828b4c9c9fba37295033cdab8
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-13 02:49:40 +0700

    Create Neural_Network.py

---
+++ pages/Neural_Network.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from buoi7.Neural_Network import Neural_Network
+if "last_page" in st.session_state and st.session_state.last_page != "Neural_Network":
+    st.session_state.clear()  # Xóa toàn bộ session
+
+st.session_state.last_page = "Neural_Network" 
+
+st.title("🔍 Thuật toán giảm chiều")
+
+
+# Gọi hàm ClusteringAlgorithms từ module
+Neural_Network()
