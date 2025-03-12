ab6f0f3e40a0e5f684754dce8da4afbdda12ddff
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-12 23:22:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ def explain_nn():
 
     ### 🔰 Kiến trúc cơ bản:
     """)
-    st.image("buoi7/img1.png", caption="Một số hình ảnh từ MNIST Dataset", use_container_width=True)
+    st.image("buoi7/img1.webp", caption="Một số hình ảnh từ MNIST Dataset", use_container_width=True)
     
     st.markdown("""
     ### 📌 Các thành phần chính:
