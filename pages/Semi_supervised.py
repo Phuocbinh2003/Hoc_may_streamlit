635c5464f814659ececce1c41240c0ce03469bcc
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2025-03-17 09:02:42 +0700

    Update Semi_supervised.py

--- pages/Semi_supervised.py
+++ pages/Semi_supervised.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from buoi8.Semi_supervised import Semi_supervised
 
+if "last_page" in st.session_state and st.session_state.last_page != "Semi_supervised":
+    st.session_state.clear()  # Xóa toàn bộ session
+
+st.session_state.last_page = "Semi_supervised" 
+
+
+
+# Gọi hàm Classification từ module
+Semi_supervised()
